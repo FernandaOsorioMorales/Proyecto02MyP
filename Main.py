0ccf09139d9061d1ae1e14938a2167c94ee4d6c9
Anshar <anshar@ciencias.unam.mx>
2022-11-23 01:24:12 -0600

    implementacion terminada, dios plan

--- Main.py
+++ Main.py
@@ -1,22 +1,25 @@
 import LecturaImagenes
-import ImagenesEjemplos
-from PIL import ImageShow
-from numpy import asarray 
-import cv2
+import colores
+from PIL import Image
 
 
 if __name__ == '__main__':
     imagen_fuente = LecturaImagenes.solicitarImagenYPasarlaAMatriz()
-    colores_fuente, fondo_fuente = LecturaImagenes.escaneoDeColores(imagen_fuente)
+    colores_fuente, fondo_fuente = colores.escaneoDeColores(imagen_fuente)
     colores_fuente = list(colores_fuente)
     for color in colores_fuente:
         figura_aislada = LecturaImagenes.aislarFigura(color, fondo_fuente, imagen_fuente)
         #figura_aislada.show()
         figura_aislada.save(f"{color}.jpg")
         figura_aisladaBN = LecturaImagenes.transformacion_imagen_opencv(f"{color}.jpg")
-        figuraContorneada = LecturaImagenes.encuentra_contorno(figura_aisladaBN)
-        vertices_figura = LecturaImagenes.obtener_vertices(figuraContorneada, figura_aislada, color)
-        print(color+" : "+str(vertices_figura))
+        figura_aislada_grande = LecturaImagenes.agrandar(figura_aisladaBN)
+        figuraContorneada = LecturaImagenes.encuentra_contorno(figura_aislada_grande)
+        imagen_control = Image.fromarray(figura_aislada_grande)
+        vertices_figura = LecturaImagenes.obtener_vertices(figuraContorneada, imagen_control, color)
+        hex_color = colores.hexadecimal(color)
+        figura_final = LecturaImagenes.tipo_de_figura(vertices_figura)
+        print(f"{hex_color} = {figura_final}")
+        
         
 
 
